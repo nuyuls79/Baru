49ae662b24db2485f85ead79a7af1ff57004796e
nuyuls79 <nuyulshare001@gmail.com>
2026-04-30 15:46:35 +0700

    Enable minification and resource shrinking for release

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -159,8 +159,8 @@ android {
         release {
             signingConfig = signingConfigs.getByName("release")
             isDebuggable = false
-            isMinifyEnabled = false 
-            isShrinkResources = false
+            isMinifyEnabled = true
+            isShrinkResources = true
             proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
         }
         debug {
