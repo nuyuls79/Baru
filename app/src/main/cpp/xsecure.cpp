3a925e115ec0beb63cf181770027705adf84fce7
nuyuls79 <nuyulshare001@gmail.com>
2026-05-01 05:53:56 -0700

    Add files via upload

--- app/src/main/cpp/xsecure.cpp
+++ app/src/main/cpp/xsecure.cpp
@@ -2,6 +2,9 @@
 #include <string>
 #include <cstring>
 #include <android/log.h>
+#include <thread>
+#include <chrono>
+#include <stdlib.h>
 
 static const std::string base64_chars =
         "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
@@ -44,6 +47,112 @@ std::string base64_decode(const std::string &encoded_string) {
     return ret;
 }
 
+// ==================== ANTI-TAMPER: CEK SIGNATURE APK ====================
+static bool isApkSignatureValid(JNIEnv* env, jobject context) {
+    // Implementasi pengecekan signature APK
+    // Jika signature tidak cocok, return false
+    // Untuk sementara kita anggap valid (bisa diimplementasikan nanti)
+    return true;
+}
+// ======================================================================
+
+// ==================== FUNGSI DETEKSI PROXY/VPN ====================
+static bool isProxyOrVpnActive(JNIEnv* env, jobject context) {
+    // 1. Cek proxy dari Settings.Global
+    jclass settingsClass = env->FindClass("android/provider/Settings$Global");
+    jmethodID getString = env->GetStaticMethodID(settingsClass, "getString",
+        "(Landroid/content/ContentResolver;Ljava/lang/String;)Ljava/lang/String;");
+    jclass contextClass = env->GetObjectClass(context);
+    jmethodID getContentResolver = env->GetMethodID(contextClass, "getContentResolver",
+        "()Landroid/content/ContentResolver;");
+    jobject contentResolver = env->CallObjectMethod(context, getContentResolver);
+    jstring httpProxy = (jstring)env->CallStaticObjectMethod(settingsClass, getString,
+        contentResolver, env->NewStringUTF("http_proxy"));
+
+    if (httpProxy != nullptr) {
+        const char* proxyStr = env->GetStringUTFChars(httpProxy, nullptr);
+        bool proxyActive = (strlen(proxyStr) > 0 && strcmp(proxyStr, ":0") != 0);
+        env->ReleaseStringUTFChars(httpProxy, proxyStr);
+        if (proxyActive) return true;
+    }
+
+    // 2. Cek VPN dari ConnectivityManager
+    jclass connectivityClass = env->FindClass("android/net/ConnectivityManager");
+    jmethodID getSystemService = env->GetMethodID(contextClass, "getSystemService",
+        "(Ljava/lang/String;)Ljava/lang/Object;");
+    jstring connectivityService = env->NewStringUTF("connectivity");
+    jobject connectivityManager = env->CallObjectMethod(context, getSystemService,
+        connectivityService);
+    env->DeleteLocalRef(connectivityService);
+
+    if (connectivityManager != nullptr) {
+        jmethodID getActiveNetwork = env->GetMethodID(connectivityClass, "getActiveNetwork",
+            "()Landroid/net/Network;");
+        jobject activeNetwork = env->CallObjectMethod(connectivityManager, getActiveNetwork);
+        if (activeNetwork != nullptr) {
+            jmethodID getNetworkCapabilities = env->GetMethodID(connectivityClass,
+                "getNetworkCapabilities",
+                "(Landroid/net/Network;)Landroid/net/NetworkCapabilities;");
+            jobject capabilities = env->CallObjectMethod(connectivityManager,
+                getNetworkCapabilities, activeNetwork);
+            if (capabilities != nullptr) {
+                jclass networkCapabilitiesClass = env->FindClass("android/net/NetworkCapabilities");
+                jmethodID hasTransport = env->GetMethodID(networkCapabilitiesClass, "hasTransport", "(I)Z");
+                jboolean hasVpn = env->CallBooleanMethod(capabilities, hasTransport, 4);
+                if (hasVpn) return true;
+            }
+        }
+    }
+    return false;
+}
+// ===================================================================
+
+// ==================== CLEAR CACHE ====================
+static void clearAllCache(JNIEnv* env, jobject context) {
+    jclass contextClass = env->GetObjectClass(context);
+    jmethodID getCacheDir = env->GetMethodID(contextClass, "getCacheDir", "()Ljava/io/File;");
+    jobject cacheDir = env->CallObjectMethod(context, getCacheDir);
+    
+    if (cacheDir != nullptr) {
+        jclass fileClass = env->FindClass("java/io/File");
+        jmethodID deleteRecursively = env->GetMethodID(fileClass, "deleteRecursively", "()Z");
+        env->CallBooleanMethod(cacheDir, deleteRecursively);
+    }
+    
+    // External cache dir (butuh permission, bisa diabaikan)
+    jmethodID getExternalCacheDir = env->GetMethodID(contextClass, "getExternalCacheDir", "()Ljava/io/File;");
+    jobject externalCacheDir = env->CallObjectMethod(context, getExternalCacheDir);
+    if (externalCacheDir != nullptr) {
+        jclass fileClass = env->FindClass("java/io/File");
+        jmethodID deleteRecursively = env->GetMethodID(fileClass, "deleteRecursively", "()Z");
+        env->CallBooleanMethod(externalCacheDir, deleteRecursively);
+    }
+}
+// ===================================================
+
+// ==================== THREAD UNTUK MONITORING REAL-TIME ====================
+static void startMonitoringThread(JNIEnv* env, jobject context) {
+    // Detach agar thread bisa berjalan mandiri
+    JavaVM* jvm;
+    env->GetJavaVM(&jvm);
+    
+    std::thread([jvm, context]() {
+        JNIEnv* monitorEnv;
+        jvm->AttachCurrentThread(&monitorEnv, nullptr);
+        
+        while (true) {
+            std::this_thread::sleep_for(std::chrono::milliseconds(2000));
+            
+            if (isProxyOrVpnActive(monitorEnv, context)) {
+                clearAllCache(monitorEnv, context);
+                jvm->DetachCurrentThread();
+                exit(0);
+            }
+        }
+    }).detach();
+}
+// =========================================================================
+
 extern "C" {
 
 static const char* ENCODED_PREMIUM_REPO = "aHR0cHM6Ly9yYXcuZ2l0aHVidXNlcmNvbnRlbnQuY29tL251eXVsczc5L1N0cmVhbVBsYXktRnJlZS9yZWZzL2hlYWRzL2J1aWxkcy9yZXBvLmpzb24=";
@@ -61,58 +170,25 @@ Java_com_lagradost_cloudstream3_utils_RepoProtector_nativeGetFreeRepoUrl(JNIEnv*
     return env->NewStringUTF(decoded.c_str());
 }
 
-// ==================== DETEKSI PROXY & VPN (ANTI-HTTPCANARY, DLL.) ====================
-JNIEXPORT jboolean JNICALL
-Java_com_lagradost_cloudstream3_MainActivity_isProxyOrVpnActive(JNIEnv* env, jclass, jobject context) {
-    // 1. Cek proxy dari Settings.Global
-    jclass settingsGlobalClass = env->FindClass("android/provider/Settings$Global");
-    jmethodID getStringMethod = env->GetStaticMethodID(settingsGlobalClass, "getString",
-        "(Landroid/content/ContentResolver;Ljava/lang/String;)Ljava/lang/String;");
-    jclass contextClass = env->GetObjectClass(context);
-    jmethodID getContentResolverMethod = env->GetMethodID(contextClass, "getContentResolver",
-        "()Landroid/content/ContentResolver;");
-    jobject contentResolver = env->CallObjectMethod(context, getContentResolverMethod);
-    jstring httpProxyJ = (jstring)env->CallStaticObjectMethod(settingsGlobalClass, getStringMethod,
-        contentResolver, env->NewStringUTF("http_proxy"));
-
-    if (httpProxyJ != nullptr) {
-        const char* proxyStr = env->GetStringUTFChars(httpProxyJ, nullptr);
-        bool proxyActive = (strlen(proxyStr) > 0 && strcmp(proxyStr, ":0") != 0);
-        env->ReleaseStringUTFChars(httpProxyJ, proxyStr);
-        if (proxyActive) return JNI_TRUE;
+// ==================== INITIAL CHECK (DIPANGGIL SEKALI DI AWAL) ====================
+JNIEXPORT void JNICALL
+Java_com_lagradost_cloudstream3_CloudStreamApp_checkAndBlock(JNIEnv* env, jobject thiz) {
+    if (!isApkSignatureValid(env, thiz)) {
+        clearAllCache(env, thiz);
+        exit(0);
     }
-
-    // 2. Cek VPN dari ConnectivityManager
-    jstring connectivityServiceName = env->NewStringUTF("connectivity");
-    jmethodID getSystemServiceMethod = env->GetMethodID(contextClass, "getSystemService",
-        "(Ljava/lang/String;)Ljava/lang/Object;");
-    jobject connectivityManager = env->CallObjectMethod(context, getSystemServiceMethod,
-        connectivityServiceName);
-    env->DeleteLocalRef(connectivityServiceName);
-
-    if (connectivityManager != nullptr) {
-        jclass connectivityManagerClass = env->FindClass("android/net/ConnectivityManager");
-        jmethodID getActiveNetworkMethod = env->GetMethodID(connectivityManagerClass,
-            "getActiveNetwork", "()Landroid/net/Network;");
-        jobject activeNetwork = env->CallObjectMethod(connectivityManager, getActiveNetworkMethod);
-        if (activeNetwork != nullptr) {
-            jmethodID getNetworkCapabilitiesMethod = env->GetMethodID(connectivityManagerClass,
-                "getNetworkCapabilities",
-                "(Landroid/net/Network;)Landroid/net/NetworkCapabilities;");
-            jobject capabilities = env->CallObjectMethod(connectivityManager,
-                getNetworkCapabilitiesMethod, activeNetwork);
-            if (capabilities != nullptr) {
-                jclass networkCapabilitiesClass = env->FindClass("android/net/NetworkCapabilities");
-                jmethodID hasTransportMethod = env->GetMethodID(networkCapabilitiesClass,
-                    "hasTransport", "(I)Z");
-                // TRANSPORT_VPN = 4
-                jboolean hasVpn = env->CallBooleanMethod(capabilities, hasTransportMethod, 4);
-                if (hasVpn) return JNI_TRUE;
-            }
-        }
+    if (isProxyOrVpnActive(env, thiz)) {
+        clearAllCache(env, thiz);
+        exit(0);
     }
-    return JNI_FALSE;
 }
-// ====================================================================================
+// ==============================================================================
+
+// ==================== START NATIVE MONITORING THREAD ====================
+JNIEXPORT void JNICALL
+Java_com_lagradost_cloudstream3_CloudStreamApp_startNativeMonitor(JNIEnv* env, jobject thiz) {
+    startMonitoringThread(env, thiz);
+}
+// =======================================================================
 
 } // extern "C"
